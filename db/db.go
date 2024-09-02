65088c669146fc46986b1c6d6cf0d4c4e8762754
Ahsan Sourav <aasourav@yahoo.com>
2024-09-02 16:19:22 +0600

    project setup

---
+++ db/db.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+
+func ConnectToMongo() (*mongo.Client, error) {
+	// MongoDb connection string
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// getting username and password from .env
+	username := os.Getenv("MONGO_DB_USERNAME")
+	password := os.Getenv("MONGO_DB_PASSWORD")
+
+	// setting auth credentials
+	clientOptions.SetAuth(options.Credential{
+		Username: username,
+		Password: password,
+	})
+
+	// Connect to mongo
+	// TODO: check either using TODO or Background
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	// var d *readpref.ReadPref
+	// client.Ping(context.Background(), d)
+	// log.Println(d.String())
+
+	log.Println(client.ListDatabaseNames(context.Background(), options.ListDatabases()))
+	log.Println("Connected to mongo...")
+
+	return client, nil
+}
